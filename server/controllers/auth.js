935f70d416888281c7f7a2c6a60a9aba82395439
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-27 16:52:57 +0545

    model and migration completed along wth assets folder

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,7 +22,20 @@ export const register = async (req, res) => {
     const passwordHash = await bcrypt.hash(password, salt);
 
     const newUser = new User({
-        
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      locaton,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
     });
-  } catch (error) {}
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
