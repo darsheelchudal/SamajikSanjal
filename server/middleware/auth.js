6ad2ef42de47a5fab326ba02e6be2149565a382b
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-27 17:32:08 +0545

    Authorization

---
+++ server/middleware/auth.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+    if (!token) {
+      return res.status(403).send("Access denied");
+    }
+    const verified = jwt.token(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.send(500).json(error.message);
+  }
+};
