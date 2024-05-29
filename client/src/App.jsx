3e941965d83e6695f20e7476c5437135a5ef2081
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-29 18:21:32 +0545

    reducer slice setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import ProfilePage from "./pages/profilePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
       <div></div>
     </>
   );
