e886f7436b12f7748010dab34ff091336d2d7d16
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-29 06:36:46 +0545

    dummy data inserted

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/dummy.js";
+
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -62,5 +66,8 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+    //Add data one time
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
